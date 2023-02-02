f548a1246e8d6d62c030c95fbe4380f0fa3a31a6
Ashwani <ashwanikushwah72@gmail.com>
2023-02-02 10:55:53 +0530

    Added documentation

--- pdfMerger.py
+++ pdfMerger.py
@@ -4,9 +4,12 @@ import os
 
 merger = PyPDF2.PdfMerger()
 
+# Getting the files from the current working directory
 for file in os.listdir(os.curdir):
     if file.endswith(".pdf"):
+        # merging the file 
         merger.append(file)
+    # Giving name to the file 
     merger.write("combinedFiles.pdf")
     
     
